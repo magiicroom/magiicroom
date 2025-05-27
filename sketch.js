e40b765529f2c89299f3b008924c0a72d75db33e
Eric <eric.koutaro@gmail.com>
2025-05-27 23:09:58 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -260,3 +260,156 @@ async function setup() {
     for (let y = 0; y < bgLineCount; y++) {
         let t = y / (bgLineCount - 1);
         let nowY = height * t;
+
+        NYSetColorLerp(bgHueA, bgSatA, bgBriA, bgHueB, bgSatB, bgBriB, t);
+        NYDotLine(0, nowY, width, nowY);
+
+        if (y % 10 == 0)
+            await sleep(1);
+    }
+    dotDensity = lastDotDensity;
+
+    let xCount = floor(random(1, 3));
+    let yCount = floor(random(1, 3));
+
+    let padding = min(width, height) * paddingRatio;
+
+    let rectWidth = (width - 2 * padding) / xCount;
+    let rectHeight = (height - 2 * padding) / yCount;
+
+    for (let x = 0; x < xCount; x++) {
+        for (let y = 0; y < yCount; y++) {
+
+            let startX = rectWidth * x + padding;
+            let startY = rectHeight * y + padding;
+
+            let plantX = startX + random(rectWidth * 0.2, rectWidth * 0.8);
+            let plantY = startY + random(rectHeight * 0.2, rectHeight * 0.8);
+
+            let plantSize = min(rectWidth, rectHeight) * random(0.3, 0.6);
+            let leafWidth = plantSize * random(0.2, 0.8);
+
+            let layers = floor(random(3, 12));
+            let countPerLayer = floor(random(3, 24));
+
+            let rects = SubdivideRect(startX, startY, rectWidth, rectHeight, 0);
+
+            for (let r = 0; r < rects.length; r++) {
+                let rectObj = rects[r];
+
+                let bowlX = rectObj.x;
+                let bowlY = rectObj.y;
+                let bowlWidth = rectObj.w;
+                let bowlHeight = rectObj.h;
+
+                let newBowl = new PlantBowl(bowlX, bowlY, bowlWidth, bowlHeight);
+                bowls.push(newBowl);
+            }
+        }
+    }
+
+    for (let i = 0; i < bowls.length; i++) {
+
+        if (bowls[i].bowlType == 3) // empty bowl
+            continue;
+
+        if (bowls[i].bowlType <= 1) // rect
+        {
+            await bowls[i].drawBowlRect();
+        }
+        else if (bowls[i].bowlType == 2) // circle
+        {
+            await bowls[i].drawBowlRound();
+        }
+
+        await sleep(1);
+    }
+
+    // sort: small bowl draw first
+    bowls.sort(function (a, b) {
+        if (a.plantSize < b.plantSize)
+            return -1;
+        else if (a.plantSize > b.plantSize)
+            return 1;
+        else
+            return 0;
+    });
+
+    for (let i = 0; i < bowls.length; i++) {
+        if (bowls[i].bowlType == 3) // empty bowl
+            continue;
+
+        bowls[i].drawPlant();
+        await sleep(1);
+    }
+}
+
+
+function SubdivideRect(_x, _y, _width, _height, _depth) {
+
+    let doSplit = random(0, 1) < 0.9;
+
+    if (_depth == 0)
+        doSplit = true;
+
+    if (min(_width, _height) < 120) {
+        doSplit = false;
+    }
+
+    if (doSplit) {
+        let splitRatio = random(0.4, 0.6);
+
+        // split X
+        if (random() < 0.5) {
+            let rectA_x = _x;
+            let rectA_y = _y;
+            let rectA_width = _width * splitRatio;
+            let rectA_height = _height;
+
+            let rectB_x = _x + _width * splitRatio;
+            let rectB_y = _y;
+            let rectB_width = _width * (1 - splitRatio);
+            let rectB_height = _height;
+
+            let rectA = SubdivideRect(rectA_x, rectA_y, rectA_width, rectA_height, _depth + 1);
+            let rectB = SubdivideRect(rectB_x, rectB_y, rectB_width, rectB_height, _depth + 1);
+
+            return rectA.concat(rectB);
+        }
+        // split Y
+        else {
+            let rectA_x = _x;
+            let rectA_y = _y;
+            let rectA_width = _width;
+            let rectA_height = _height * splitRatio;
+
+            let rectB_x = _x;
+            let rectB_y = _y + _height * splitRatio;
+            let rectB_width = _width;
+            let rectB_height = _height * (1 - splitRatio);
+
+            let rectA = SubdivideRect(rectA_x, rectA_y, rectA_width, rectA_height, _depth + 1);
+            let rectB = SubdivideRect(rectB_x, rectB_y, rectB_width, rectB_height, _depth + 1);
+
+            return rectA.concat(rectB);
+        }
+    }
+    else {
+        return [{ x: _x, y: _y, w: _width, h: _height, depth: _depth }];
+    }
+}
+
+function draw() {
+    // Your draw loop if needed for animation, currently empty.
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function keyPressed(e) {
+    if (e.key == 's') {
+        saveCanvas(`succulent-${width}-${height}-${fxhash}.png`);
+    }
+}
